9c415999a9ff8ffe3edff15e2ffdce95230549f0
Henrique Fontenele <henriquefontenele078@gmail.com>
2023-10-31 11:52:22 -0300

    Add Prisma to Template

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import Express from "express";
+
+const app = Express();
+app.use(Express.json());
+
+const PORT = 8000;
+
+app.get("/", (request, response) => {
+	return response.send({message: "Hello World!"});
+});
+
+app.listen(PORT, () => {
+	console.log("Server is running in port " + PORT);
+});
