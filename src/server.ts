0a0d20a28fa3cd60838eca70915bda388baabfe2
Henrique Fontenele <101604488+Henrique0078@users.noreply.github.com>
2024-02-28 11:47:25 -0300

    FEAT: Error Message

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,20 @@ app.use(Express.json());
 
 const PORT = 8000;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof Error) {
+		return res.status(400).json({
+			error: err.message,
+		});
+	}
+
+	return res.status(500).json({
+		status: "error",
+		message: "Internal Server Error",
+	});
+});
+
 app.get("/", (request, response) => {
 	return response.send({message: "Hello World!"});
 });
